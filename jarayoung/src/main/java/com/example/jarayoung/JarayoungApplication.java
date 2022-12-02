00e98a6b04b11d1595408c9e33ec0e82e0e00703
LEEJiHo <destiny3910@gmail.com>
2022-12-02 21:06:51 +0900

    project init

---
+++ jarayoung/src/main/java/com/example/jarayoung/JarayoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.jarayoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarayoungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarayoungApplication.class, args);
+    }
+
+}
